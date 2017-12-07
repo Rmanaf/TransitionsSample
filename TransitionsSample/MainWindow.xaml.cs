5255acfe588c21bc283451c7239b67a723a4c992
Arman Afzal <rman.afzal@gmail.com>
2017-12-07 12:20:03 +0330

    0.2.1
    Description added

--- TransitionsSample/MainWindow.xaml.cs
+++ TransitionsSample/MainWindow.xaml.cs
@@ -5,31 +5,32 @@ using System.Windows.Media;
 
 namespace TransitionsSample
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-
-
-        private TranslateTransform EllipseTransform = new TranslateTransform();
 
         public MainWindow()
         {
             InitializeComponent();
 
+
+            // CompositionTarget as timer to render frames
             CompositionTarget.Rendering += CompositionTarget_Rendering;
 
+
+            // define and assign transform object
             TranslateTransform EllipseTransform = new TranslateTransform();
             EllipseTarget.RenderTransform = EllipseTransform;
             EllipseTransform.X = -100;
 
+            // tween
             TransitionFactory.Transform(EllipseTransform, "X", Divankits.Transitions.Easing.Elastic.EaseInOut, EllipseTransform.X, 100, 2.5)
                 .OnFinished += (s, evt) => { (s as Tween).Yoyo(); };
 
 
         }
 
+        // frame update method
         private void CompositionTarget_Rendering(object sender, EventArgs e)
         {
 
