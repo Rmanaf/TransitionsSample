893a5070a7f1c2469cd642a2f93e37648fbf9db9
Arman Afzal <rman.afzal@gmail.com>
2017-12-21 20:47:51 +0330

    1.0.0
    Motion class problem solved

--- TransitionsSample/MainWindow.xaml.cs
+++ TransitionsSample/MainWindow.xaml.cs
@@ -9,8 +9,11 @@ namespace TransitionsSample
     public partial class MainWindow : Window
     {
 
+
+
         public MainWindow()
         {
+
             InitializeComponent();
 
 
@@ -23,9 +26,55 @@ namespace TransitionsSample
             EllipseTarget.RenderTransform = EllipseTransform;
             EllipseTransform.X = -100;
 
-            // tween
-            TransitionFactory.Transform(EllipseTransform, "X", Divankits.Transitions.Easing.Elastic.EaseInOut, EllipseTransform.X, 100, 2.5)
-                .OnFinished += (s, evt) => { (s as Tween).Yoyo(); };
+
+
+
+            /* Sample 1 :
+                Transform one property
+            */
+
+            //(TransitionFactory.Transform(EllipseTransform, "X", Divankits.Transitions.Easing.Elastic.EaseInOut, EllipseTransform.X, 100, 2.5) as Tween)
+            //    .OnFinished += (s, evt) => { (s as Tween).Yoyo(); };
+
+
+
+
+
+            /* Sample 2 :
+                Transform multiple properties using one transformation method
+            */
+
+            //var motion = (TransitionFactory.Transform(EllipseTransform,
+            //    new string[] { "X", "Y" },
+            //    Divankits.Transitions.Easing.Elastic.EaseInOut,
+            //    new double[] { EllipseTransform.X, EllipseTransform.Y },
+            //    new double[] { 100, 80 },
+            //    new double[] { 2.5, 4 }) as Motion);
+
+            //motion.GetLongestTransform().OnFinished += (s, evt) => motion.Yoyo();
+
+
+
+
+
+
+            /* Sample 3 :
+                Transform multiple properties
+            */
+
+            var motion = (TransitionFactory.Transform(EllipseTransform,
+                new string[] { "X", "Y" },
+                new TransitionParameters.Method<double, double, double, double, double>[] {
+                    Divankits.Transitions.Easing.Elastic.EaseInOut,
+                    Divankits.Transitions.Easing.Bounce.EaseOut,
+                },
+                new double[] { EllipseTransform.X, EllipseTransform.Y },
+                new double[] { 100, 80 },
+                new double[] { 2.5, 4 }) as Motion);
+
+            motion.GetLongestTransform().OnFinished += (s, evt) => motion.Yoyo();
+
+
 
 
         }
